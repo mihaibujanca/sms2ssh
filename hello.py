986699775517591cb0983b6acb9263f0c9ceaeed
CristinaCristescu <bianca-cristina.cristescu@cern.ch>
2014-11-01 12:27:41 +0000

    WHatever helps!

--- hello.py
+++ hello.py
@@ -77,10 +77,35 @@ class YourClass:
 
 newcl = YourClass()
 
+
+from flask import Flask, session, redirect, url_for, escape, request
+
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    if not session.has_key('phonenumber'):
+        session['phonenumber'] = 'Save in session'    
+        return "Session value set."
+    else:
+        return session['phonenumber']
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        session['phonenumber'] = request.form['phonenumber']
+        return redirect(url_for('index'))
+
+@app.route('/logout')
+def logout():
+    # remove the username from the session if it's there
+    session.pop('phonenumber', None)
+    return redirect(url_for('index'))
+
+# set the secret key.  keep this really secret:
+app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
+
+#@app.route('/', methods=['GET', 'POST'])
 def hello_monkey():
     message = ''
     number = request.values.get('From', None)
